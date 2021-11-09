55b9f8efcb36585f19628b9c410245f43b06177d
Pierre-Louis Biojout <plbiojout@MacBook-Pro-de-Pierre-Louis.local>
2021-11-09 16:30:12 +0100

    structure

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ This is the main script, which us functions from modules.
 
 Theta (global param) is a list of dictionnaries.
 """
-from modules import *
+from modules import * #TODO syntax
 
-def train(data, agents_matrix, agents_data_idx, privacy):
+def train(data, agents_matrix, agents_data_idx, privacy, nb_steps):
     """
     random initialization
-    for each step
+    for each step in range nb_steps :
         for each agent:
             if agent wakes up 
                 update local theta_i (4)
                 compute new local param
                 broadcast step
                 calculate time before next wake up (random.poisson(lam=1.0, size=None))
-            compute new local param (1)
     """
     return False
 
--- modules.py
+++ modules.py
@@ -5,6 +5,14 @@ This is where we put functions
 def construct_matrix(data): # construct the adjency matrix of the graph, using method from paper for ml-100k
     return False
 
+#Update step
+def computeGrad():
+    return False
+
+#broadcast step
+def broadcast(model, newTheta, matrix):
+    return False
+
 class agent : #idx, matrix, model?
 
     def computeLocalParam(self, data, model, steps): #update local theta
